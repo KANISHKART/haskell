3309eb2a19c48e0ffcc0603a0a66dd4cba15a11c
kanishkar <kanishkrish123@gmail.com>
2024-01-05 18:57:46 +0000

    method signature

--- square.hs
+++ square.hs
@@ -8,6 +8,7 @@
 --below is a recursion to check number is a sqrt 
 --iterating from 1 to sqrt input which user had given
 --if multiple itself equals, goes above or sqrt equals iteration will exit the recursion
+chkSquare::Int->Int->Bool
 chk_square x y
     | x < 0 = False
     | x == 1 || x==0 = True
@@ -15,4 +16,5 @@ chk_square x y
     | (y*y > x) = False
     | otherwise = chk_square x (y+1)
 
+is_square::Int->Bool
 is_square x = chk_square x 1
